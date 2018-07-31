b3822ee10c2dfe1688755e76c913bd36cfa681f4
watal <watal.i27e@gmail.com>
2018-07-31 14:22:37 +0900

    Remove unused struct

--- procout-mac.c
+++ procout-mac.c
@@ -32,39 +32,6 @@
 #define PTRACE_PEEKDATA PT_READ_D
 #endif
 
-#ifndef __linux__
-struct user_regs_struct
-{
-  __extension__ unsigned long long int r15;
-  __extension__ unsigned long long int r14;
-  __extension__ unsigned long long int r13;
-  __extension__ unsigned long long int r12;
-  __extension__ unsigned long long int rbp;
-  __extension__ unsigned long long int rbx;
-  __extension__ unsigned long long int r11;
-  __extension__ unsigned long long int r10;
-  __extension__ unsigned long long int r9;
-  __extension__ unsigned long long int r8;
-  __extension__ unsigned long long int rax;
-  __extension__ unsigned long long int rcx;
-  __extension__ unsigned long long int rdx;
-  __extension__ unsigned long long int rsi;
-  __extension__ unsigned long long int rdi;
-  __extension__ unsigned long long int orig_rax;
-  __extension__ unsigned long long int rip;
-  __extension__ unsigned long long int cs;
-  __extension__ unsigned long long int eflags;
-  __extension__ unsigned long long int rsp;
-  __extension__ unsigned long long int ss;
-  __extension__ unsigned long long int fs_base;
-  __extension__ unsigned long long int gs_base;
-  __extension__ unsigned long long int ds;
-  __extension__ unsigned long long int es;
-  __extension__ unsigned long long int fs;
-  __extension__ unsigned long long int gs;
-};
-#endif
-
 void peek_and_output(pid_t pid, long long addr, long long size, int fd)
 {
     // 標準出力か標準エラー出力
